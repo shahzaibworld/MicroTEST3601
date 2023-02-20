05b804b9e22742bc25e5228fadd5e149c5966e8b
shahzaibworld <125930100+shahzaibworld@users.noreply.github.com>
2023-02-21 03:54:54 +0500

    Color change

--- clock.py
+++ clock.py
@@ -1,13 +1,13 @@
 from tkinter import Label, Tk 
 import time
 app_window = Tk() 
-app_window.title("Digital Clock") 
+app_window.title("Digital-Clock") 
 app_window.geometry("420x150") 
 app_window.resizable(1,1)
 
 text_font= ("Boulder", 68, 'bold')
-background = "#f2e750"
-foreground= "#363529"
+background = "#cbd6d5"
+foreground= "#02f74c"
 border_width = 25
 
 label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width) 
